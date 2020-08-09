289f4a5bc17115328ac6a581c3ca63940ee86481
Vicente Almeida <vicentedealmeidaluiz@gmail.com>
2020-08-09 01:41:33 -0300

    Acrescentando outra frase

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -4,6 +4,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Bom dia seu Vicente");
+		System.out.println("Bom dia seu Vicente de Almeida Luiz");
 		
 
 	}
